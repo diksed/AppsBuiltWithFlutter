a09c54becfa4efe176c60beb56f7519f49b2db88
diksed <diksed00@gmail.com>
2024-06-12 20:46:04 +0300

    href added to index

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "365572698"
+    serviceWorkerVersion: "1662177238"
   }
 });
