368fa73eb4584b2ccb9f900307ec5c8e0f569ad4
diksed <diksed00@gmail.com>
2024-06-13 19:02:11 +0300

    firebase analytics added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1577090221"
+    serviceWorkerVersion: "1187647014"
   }
 });
