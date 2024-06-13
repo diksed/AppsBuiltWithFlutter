e1beeec904f2c4f54592bf5da3a6abd8a65c3ebd
diksed <diksed00@gmail.com>
2024-06-13 20:22:05 +0300

    firebase config added

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyDKOhf8u9rEUwKbec9Bfvu2DwAwjnWUlK0",
+    authDomain: "appsbuiltwithflutter-db.firebaseapp.com",
+    projectId: "appsbuiltwithflutter-db",
+    storageBucket: "appsbuiltwithflutter-db.appspot.com",
+    messagingSenderId: "960098476401",
+    appId: "1:960098476401:web:6af8923478c0aecbef96d0",
+    measurementId: "G-S65EKQNG7V"
+};
