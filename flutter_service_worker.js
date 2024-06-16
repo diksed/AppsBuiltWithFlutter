a6529ce4f6d731aa2054b2a0b955b44f7899604d
diksed <diksed00@gmail.com>
2024-06-16 10:23:42 +0300

    AdvertisingDialog changed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,14 +28,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "373234d664ffe656d0fb4039a86ad513
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase_config.js": "7d5957f0ee0347c362990f65f7c58632",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f909f962380ae867e9675a4e8cbb8c6a",
+"flutter_bootstrap.js": "49b7ed4be3b73ec050201162c84500a1",
 "icons/icon-192.png": "a4d9503f57c1a53706ddf9762c2298b0",
 "icons/icon-512.png": "45ac3e30af819917a2202e46c53c4e51",
 "icons/Icon-maskable-192.png": "bc18306153b5a496ce63506c08271881",
 "icons/Icon-maskable-512.png": "bf5dc2e7b484c0971ea860c6aae996eb",
 "index.html": "076ead7f813503c50ad80760f15d1ca3",
 "/": "076ead7f813503c50ad80760f15d1ca3",
-"main.dart.js": "bf8884ad5792813d6070e7263d772fd8",
+"main.dart.js": "0744ed555e87ce58b0018090059058b1",
 "manifest.json": "e960e7fbb624d0037ef621e7fdecb7e4",
 "version.json": "5f1ef8db5b71fdaf12b905b33cc85cd0"};
 // The application shell files that are downloaded before a service worker can
